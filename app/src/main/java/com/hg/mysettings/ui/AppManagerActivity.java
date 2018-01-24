3e53f9e3b101a5312fbf8296787adac893b46514
slkk <644187060@qq.com>
2018-01-24 18:33:21 +0800

    init app manager

---
+++ app/src/main/java/com/hg/mysettings/ui/AppManagerActivity.java
@@ -0,0 +1,15 @@
+package com.hg.mysettings.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hg.mysettings.R;
+
+public class AppManagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_manager);
+    }
+}
