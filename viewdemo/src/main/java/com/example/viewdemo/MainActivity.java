c0fa6ff03707b7f67858ee61b5a4c2304d025f02
slkk <644187060@qq.com>
2018-01-31 11:13:43 +0800

    complete view domo

--- viewdemo/src/main/java/com/example/viewdemo/MainActivity.java
+++ viewdemo/src/main/java/com/example/viewdemo/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.viewdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LayoutInflater layoutInflater = LayoutInflater.from(this);
+        LinearLayout linearlayout = findViewById(R.id.layout_main);
+        Button button = (Button) layoutInflater.inflate(R.layout.layout_button, null);
+        linearlayout.addView(button);
     }
 }
